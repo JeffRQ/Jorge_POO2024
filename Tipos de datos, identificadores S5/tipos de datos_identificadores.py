465808f8f2c917ffdeb4bda061d520f3f57a5df7
ASUS VIVOBOOK PRO 15 <jj.ramirezq@uea.edu.ec>
2024-06-27 11:18:39 -0500

    POO semana 5

---
+++ Tipos de datos, identificadores S5/tipos de datos_identificadores.py
@@ -0,0 +1,27 @@
+# Programa: Calculadora de Área de Triángulo
+# Descripción: Este programa calcula el área de un triángulo dados la base y la altura.
+
+def calcular_area_triangulo(base, altura):
+    """"
+    Calcula el área de un triángulo.
+
+    Args:
+        base (float): La base del triángulo.
+        altura (float): La altura del triángulo.
+
+    Returns:
+        float: El área calculada del triángulo.
+    """
+    area = (base * altura) / 2
+    return area
+
+if __name__ == "__main__":
+    # Solicitar al usuario la base y la altura del triángulo
+    base = float(input("Ingrese la base del triángulo: "))
+    altura = float(input("Ingrese la altura del triángulo: "))
+
+    # Calcular el área del triángulo
+    area_triangulo = calcular_area_triangulo(base, altura)
+
+    # Mostrar el resultado
+    print(f"El área del triángulo con base {base} y altura {altura} es: {area_triangulo}")
